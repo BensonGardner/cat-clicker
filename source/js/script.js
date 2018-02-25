25f7c664781223e38a5599541ef40defc4c4edf6
bensongardner <bensongardner@yahoo.com>
2018-02-24 23:06:48 -0600

    reconfig: Add select-a-cat functionality

--- source/js/script.js
+++ source/js/script.js
@@ -1,66 +1,61 @@
 var clickZone = $('#clickZone'),
     cats = [];
 
+// Cat constructor. This creates a 
+// cat object and pushes it into the main
+// area of the page. It also creates a menu
+// link.
+// The Title case method was found at 
+// https://stackoverflow.com/questions/5086390/jquery-title-case
+
 var Cat = function(name, picture) {
-    // var thisCat = this;
-    // console.log(thisCat);
     this.clicks = 0;
-    this.name = name;
-    var nameHTML = '<div class="cat-name"><h2>' + name +
+    this.name = name.replace(/(?:^|\s)\w/g, function(match) {
+        return match.toUpperCase();
+    });
+    var nameHTML = '<div class="cat-name"><h2>' + this.name +
             '</h2></div>',
         
-        rewardHTML = '<div class="reward" id="' + name + 
-            '-reward"><span id="' + name + 
+        rewardHTML = '<div class="reward" id="show-' + this.name + 
+            '-reward"><span id="' + this.name + 
             '-reward-text">' + this.clicks + '</span></div>', 
         
-        picHTML = '<div class="cat-pic" id="' + name + '-pic"><img alt="cat" src="' + 
-            picture + '"></div>';
+        picHTML = '<div class="cat-pic" id="show-' + this.name + '-pic"><img alt="cat" src="' + 
+            picture + '"></div>',
+        
+        menuHTML = '<a class="menu-link" href="#" id="show-' + this.name + '">' + this.name + '</a></br>';
     
-    clickZone.append('<div class="cat-box" id="' + name + '">' + nameHTML + picHTML + rewardHTML);
+    clickZone.append('<div class="cat-box" id="box-show-' + this.name + '">' + nameHTML + picHTML + rewardHTML);
     
-    var thisCatCircle = document.getElementById(name + '-reward');
-    console.log(thisCatCircle.style);
-    var thisCatCount = this.count.bind(this, thisCatCircle);
-    
-    //I"m not sure what "this" would be in the next line.
-    // Maybe I should be using bind or call for this.
-    // I  believe it's working not sure.
-    // $('#' + this.name).click(console.log(thisCat));
-    $('#' + this.name).click(thisCatCount);
     cats.push(this);
+    $('#navigation').append(menuHTML);
 };
 
+// Add the count function. It takes each cat's
+// circle as an argument, then increments the 
+// count when it is clicked.
 Cat.prototype.count = function(circle) {
-        console.log('this is ' + this);
-        console.log('this.name is ' + this.name);
-        console.log(circle);
-    
-        this.clicks++;
-        circle.style.display = 'inline';
-        circle.innerHTML = '<span id="' + this.name + '-reward-text">' + this.clicks + '</span>';
-        this.rewardText = document.getElementById(this.name + '-reward-text');
-    
-  /*      circle.style.top = (cats.indexOf[this.name] * 2) + 'vw'; */
-    
-      /*  switch (this.clicks) {  
-            case 1: 
-                circle.style.right = '10vw';
-                break;
-            case 10: 
-                circle.style.right = '8vw';
-                break;
-            case 100: 
-                circle.style.right = '6vw';
-                break; 
-            default:
-                break;
-        }*/
+    this.clicks++;
+    circle.style.display = 'inline';
+    circle.innerHTML = '<span id="' + this.name + '-reward-text">' +    this.clicks + '</span>';
 };
 
-var jen = new Cat('Jennifer', 'images/kitten-autumn-500.jpg'),
+var jen = new Cat('jennifer', 'images/kitten-autumn-500.jpg'),
     mark = new Cat('Mark', 'images/cat-grey-500.jpg');
-// someday push into array automatically
-    
 
-
-
+// Add the click-to-select functionality to the menu,
+// and the click-to-count functionality to the cat pictures
+// that the menu brings up.
+for (i = 0; i < cats.length; i++) {
+    (function(i) {
+        var catCircle = document.getElementById('show-' + 
+            cats[i].name + '-reward');
+        $('#show-' + cats[i].name).click(function() {
+            $('.cat-box').css('display', 'none');
+            $('#box-' + this.id).css('display', 'inline');
+            $('#' + this.id + '-pic').click(function() {
+                cats[i].count.call(cats[i], catCircle);
+            });
+        })
+    }).call(cats[i], i);
+}
